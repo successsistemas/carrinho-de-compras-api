8ae2a1a15fab771f59e0e2591b1e3e18514e41fe
Marcelin <marceloaugusto10123@gmail.com>
2022-05-02 12:37:01 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import configuracao from './config/configuracao';
 import { CriptoController } from './cripto/cripto.controller';
 import { CriptoModule } from './cripto/cripto.module';
+import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CriptoModule],
+  imports: [AuthModule, UsersModule, CriptoModule, DatabaseModule, ConfigModule.forRoot({
+    load: [configuracao],
+    isGlobal: true
+  })],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,13 @@
+import { ApiDatabaseService } from './api-database.service';
+import { Module } from '@nestjs/common';
+import { SiteSuccessDatabaseService } from 'src/database/site-success-database.service';
+import { SuccessDatabaseService } from 'src/database/success-database.service';
+import { FornecedorDatabaseService } from './fornecedor-database.service';
+import { DatabaseCotacaoService } from './database-cotacao.service';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+    providers: [ApiDatabaseService, DatabaseCotacaoService, SiteSuccessDatabaseService, SuccessDatabaseService, FornecedorDatabaseService, ConfigService],
+    exports: [ApiDatabaseService, DatabaseCotacaoService, SiteSuccessDatabaseService, SuccessDatabaseService, FornecedorDatabaseService,]
+})
+export class DatabaseModule { }
