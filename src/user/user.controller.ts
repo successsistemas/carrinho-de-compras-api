22dc8ad736f6b487708e386deb54597e945c9ad8
Marcelin <marceloaugusto10123@gmail.com>
2022-06-06 12:48:14 -0300

    'update'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,61 +3,39 @@ import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 @Controller("user")
 export class UserController {
 
-	merchant = {
-		"id": 3,
-		"userId": 2,
-		"date": "2020-03-01T00:00:02.000Z",
-		"products": [
-			{
-				"url": "https://t2.rg.ltmcdn.com/pt/posts/3/8/0/francesinha_a_moda_do_porto_2083_600.jpg",
-				"title": "Francesinha",
-				"productId": 1,
-				"quantity": 2
-			},
-			{
-				"url": "https://t2.rg.ltmcdn.com/pt/posts/3/8/0/francesinha_a_moda_do_porto_2083_600.jpg",
-				"title": "Francesinha",
-				"productId": 1,
-				"quantity": 2
-			},
-			{
-				"url": "https://t2.rg.ltmcdn.com/pt/posts/3/8/0/francesinha_a_moda_do_porto_2083_600.jpg",
-				"title": "Francesinha",
-				"productId": 1,
-				"quantity": 2
-			},
-			{
-				"url": "https://t2.rg.ltmcdn.com/pt/posts/3/8/0/francesinha_a_moda_do_porto_2083_600.jpg",
-				"title": "Macarrão",
-				"productId": 1,
-				"quantity": 5
-			},
-		],
-		"__v": 0
+	//variavel simulando o banco, ela vai ficar aqui até a aplicação ser encerrada, fora isso os valores vão continuar
+	cart = {
+		userId: 3,
+		date: 2019 - 12 - 10,
+		products: [{}]
 	}
 
-	cart =       {
-		userId:3,
-		date:2019-12-10,
-		products:[
-			{
-				"url": "https://t2.rg.ltmcdn.com/pt/posts/3/8/0/francesinha_a_moda_do_porto_2083_600.jpg",
-				"title": "Novo Produto",
-				"productId": 3,
-				"quantity": 5
-			},
-		]
-	}
+
+	//rota que retorna o carrinho
 
 	@Get('cart/:userId')
 	getUserCart(@Param('userId') userId: string) {
 
-		return this.merchant;
+		return this.cart;
 	}
 	@Post('cart/:userId/:cartId')
-	postToCard(@Query('userId/cartId')cardId: any, userId:string) {
-		console.log(cardId)
-		return this.cart
+	adicionarProdutoAoCarrinho(@Query('userId/cartId') cardId: string, userId: string, @Body() body: any) {
+
+		//primeiro item ta vazio por causa do {}
+	
+
+		//produto recebido pelo body
+		const produto:any = {
+			url: body.produtoimage,
+			title: body.productname,
+			productId: 1,
+			quantity: 5
+		}
+
+		//adicionando produto no carrinho - equivalente ao insert into 
+		this.cart.products.push(produto)
+
+		return produto
 	}
 
 
