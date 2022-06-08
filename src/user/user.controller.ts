d338a18b8534644ed0f4fee7ffe08c5af1c3eacf
Marcelin <marceloaugusto10123@gmail.com>
2022-06-08 13:24:09 -0300

    'end-point-delete-adicionado'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
+import e from "express";
 
 @Controller("user")
 export class UserController {
 
+	
+
 	//variavel simulando o banco, ela vai ficar aqui até a aplicação ser encerrada, fora isso os valores vão continuar
 	cart = {
 		userId: 3,
-		date: 2019 - 12 - 10,
-		products: [{}]
+		date: "2019 - 12 - 10",
+		products: [{
+			url: 'teste',
+			title: 'testes',
+			productId: 21,
+			quantity: 3
+		}]
 	}
 
 
@@ -15,40 +23,54 @@ export class UserController {
 
 	@Get('cart/:userId')
 	getUserCart(@Param('userId') userId: string) {
-
+	
 		return this.cart;
+
 	}
 	@Post('cart/:userId/:cartId')
 	adicionarProdutoAoCarrinho(@Query('userId/cartId') cardId: string, userId: string, @Body() body: any) {
 
 		//primeiro item ta vazio por causa do {}
-	
+
 
 		//produto recebido pelo body
-		const produto:any = {
+		const produto: any = {
 			url: body.produtoimage,
 			title: body.productname,
-			productId: 1,
+			productId: body.productId,
 			quantity: 5
 		}
 
-		//adicionando produto no carrinho - equivalente ao insert into 
 		
+
 		this.cart.products.push(produto)
 		return produto
 	}
 
-	@Put('removerproduto/:userId/:cartId')
-	removerProdutoDoCarrinho(@Query('userId/cartId') cardId: string, userId: string, @Body() body: any) {
-		const produto:any = {
-			url: body.produtoimage,
-			title: body.productname,
-			productId: 1,
-			quantity: 5
-		}
-		this.cart.products.splice(produto)
-		return produto
+	@Delete('removerproduto/:userId/:idProduct')
+	removerProdutoDoCarrinho(@Param() params: any) {
+		
+		
+		//cria uma nova lista
+		let novalIstaProdutos = []
+
+		//adiciona todos os elementos do cart.products á ela
+		novalIstaProdutos = this.cart.products.filter(function(produto) {
+			
+			//exceto quando essa condição for false, ou seja o id do parameteo é  == ao que vc quer remover
+			const itematualIsIgualAoParametro = produto.productId !==  Number.parseInt(params.idProduct)
+			
+			//agora vai remover
+			return itematualIsIgualAoParametro
+		})
+
+		//lista criada
+
+		//coloca essa nova lista com o id removido no lugar da products
+		this.cart.products = novalIstaProdutos;
+
+	
+		
+
 	}
-
-
 }
