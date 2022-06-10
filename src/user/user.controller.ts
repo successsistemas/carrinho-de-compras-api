67d65c901d651d7faf1eeb6ca22512d91a7cb589
Marcelin <marceloaugusto10123@gmail.com>
2022-06-10 17:23:33 -0300

    try catch

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,7 @@ import e from "express";
 import { UserService } from "./user.service";
 @Controller("user")
 export class UserController {
-	constructor(private UserService: UserService) {}
+	constructor(private UserService: UserService) { }
 
 	//variavel simulando o banco, ela vai ficar aqui até a aplicação ser encerrada, fora isso os valores vão continuar
 	cart = {
@@ -25,17 +25,25 @@ export class UserController {
 	}
 
 	@Get('cart/:userId')
-	getAllCartsByUserId(@Param('userId', ParseIntPipe) id:any) {
-		return this.UserService.findById(id).catch((e:any) => {
+	getAllCartsByUserId(@Param('userId', ParseIntPipe) id: any) {
+		return this.UserService.findById(id).catch((e: any) => {
 			throw new NotFoundException(e.message);
-		  });
+		});
 	}
 
+	//essa
 	@Post('cart/:userId/:cartId')
-	addProductToCart(@Query('userId/cartId') query : any, @Body() body: any) {
-		return this.UserService.postToCart(body);
+	async addProductToCart(@Query('userId/cartId') query: any, @Body() body: any) {
+		try {
+			await this.UserService.postToCart(body);
+			return { msg: "sucesso" }
+		} catch (e: any) {
+			return { msg: "error:"+e.message }
+		}
+
+
 	}
-	
+
 	@Post('carts/:userId/:cartId')
 	adicionarProdutoAoCarrinho(@Query('userId/cartId') cardId: string, userId: string, @Body() body: any) {
 
@@ -79,8 +87,8 @@ export class UserController {
 		this.cart.products = novalIstaProdutos;
 	}
 
-	@Delete('removerprodutos/:userId/:idProduct') 
-	removerProdutodoCart(@Param() params:any) {
+	@Delete('removerprodutos/:userId/:idProduct')
+	removerProdutodoCart(@Param() params: any) {
 		return this.UserService.removeItemFromCart(params)
 	}
 }
