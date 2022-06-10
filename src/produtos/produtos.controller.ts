bafac27078ddbfc8a01868b3363568b3130b30b6
Marcelin <marceloaugusto10123@gmail.com>
2022-06-10 09:02:20 -0300

    'alteração-rota-produtos'

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,15 +1,15 @@
 import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Query } from "@nestjs/common";
 import { ProdutosService } from "./produtos.service";
 
-@Controller()
+@Controller('produtos')
 export class ProdutosController {
     constructor(private ProdutosService: ProdutosService) {}
-    @Get('produtos')
+    @Get()
 	GetAllProducts(@Body() body: any, @Query() params:any) {
 		return this.ProdutosService.getAllProducts()
 	}
   
-	@Get('produtos/:id')
+	@Get('/:id')
 	findProductsById(@Param('id', ParseIntPipe) id:any) {
 	  return this.ProdutosService.findById(id).catch((e:any) => {
 		throw new NotFoundException(e.message);
