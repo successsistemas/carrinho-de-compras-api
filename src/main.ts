81c04b1e557ceb09d40d74656a5c805dacb8ad37
Marcelin <marceloaugusto10123@gmail.com>
2022-06-15 16:25:07 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PsORT || 3001);
+  await app.listen(process.env.PasRT || 3001);
 }
 bootstrap();
