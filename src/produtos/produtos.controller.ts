6d3718663692811fb6e6567ee1ac4385994f081b
Marcelin <marceloaugusto10123@gmail.com>
2022-06-27 12:44:07 -0300

    endpoint buscar produto por id

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -3,16 +3,25 @@ import { ProdutosService } from "./produtos.service";
 
 @Controller()
 export class ProdutosController {
-    constructor(private ProdutosService: ProdutosService) {}
-    @Get('produtos')
-	GetAllProducts(@Body() body: any, @Query() params:any) {
+	constructor(private ProdutosService: ProdutosService) { }
+	@Get('produtos')
+	GetAllProducts(@Body() body: any, @Query() params: any) {
 		return this.ProdutosService.getAllProducts()
 	}
-  
-	@Get('produtos/:id')
-	findProductsById(@Param('id', ParseIntPipe) id:any) {
-	  return this.ProdutosService.getProductsFromEmpresa(id).catch((e:any) => {
-		throw new NotFoundException(e.message);
-	  });
+
+	@Get('produtos/:idEmpresa')
+	findProductsByIdEmpresa(@Param('idEmpresa', ParseIntPipe) id: any) {
+		return this.ProdutosService.getProductsFromEmpresa(id).catch((e: any) => {
+			throw new NotFoundException(e.message);
+		});
+	}
+
+	@Get('produtos/:idEmpresa/:idProduct')
+	findProductbyId(@Param() productId: any, idEmpresa: any, id:any) {
+		return this.ProdutosService.getProductsbyId(productId, id).catch((e:any) => {
+			throw new NotFoundException(e.message);
+		})
+			
+		
 	}
 }
--- src/produtos/produtos.service.ts
+++ src/produtos/produtos.service.ts
@@ -13,18 +13,18 @@ export class ProdutosService {
 		const [rows] = await db.raw(`SELECT * FROM produto WHERE produto.empresa_id = 1`);
 		return rows;
 	}
-	// async findById(id: number) {
-	// 	const absolutepath = path.resolve('./src/produtos/produtos.json')
-	// 	const bufferJson:Buffer = readFileSync(absolutepath);
-	// 	const products = JSON.parse(bufferJson.toString());
-	// 	const todos = products.find((title:any) => title?.id === id);
+	async findById(id: number) {
+		const absolutepath = path.resolve('./src/produtos/produtos.json')
+		const bufferJson:Buffer = readFileSync(absolutepath);
+		const products = JSON.parse(bufferJson.toString());
+		const todos = products.find((title:any) => title?.id === id);
 	
-	// 	if (!todos) {
-	// 	  throw Error(`Produto com o ID '${id}' não encontrado.`);
-	// 	}
+		if (!todos) {
+		  throw Error(`Produto com o ID '${id}' não encontrado.`);
+		}
 	
-	// 	return todos;
-	//   }
+		return todos;
+	  }
 
 	  async getProductsFromEmpresa(id: number) {
 		const db = this.DatabaseService.getConnection();
@@ -34,5 +34,15 @@ export class ProdutosService {
 		}	
 		return rows;
 	}
+
+	async getProductsbyId(body:any, idEmpresa:number) {
+		const db = this.DatabaseService.getConnection();
+		const [rows] = await db.raw(`select produto.id, nome, produto.image, descricao, preco, produtocol from produto where produto.id = ${body.idProduct}`);
+		if(rows == 0) {
+			throw Error(`Produtos da empresa com o ID '${body.idProduct}' não foram encontrados.`);
+		}	
+		return rows;
+	}
+	
   
 }
