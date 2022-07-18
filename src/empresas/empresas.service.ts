96be253dadd22a95d262b5d2d804c91624d2385f
Marcelin <marceloaugsuto10123@gmail.com>
2022-07-18 10:22:48 -0300

    update

--- src/empresas/empresas.service.ts
+++ src/empresas/empresas.service.ts
@@ -12,16 +12,5 @@ export class EmpresasService {
 		const [rows] = await db.raw(`SELECT empresa.id, title, description, image, cnpj, sponor, phone FROM empresa INNER JOIN usuario WHERE usuario.id= 1 ORDER BY title`);
 		return rows;
 	}
-	async findById(id: number) {
-		const absolutepath = path.resolve('./src/empresas/empresas.json')
-		const bufferJson: Buffer = readFileSync(absolutepath);
-		const empresas = JSON.parse(bufferJson.toString());
-		const todos = empresas.find((title: any) => title?.id === id);
-
-		if (!todos) {
-			throw Error(`Produto com o ID '${id}' não encontrado.`);
-		}
-
-		return todos;
-	}
+	
 }
